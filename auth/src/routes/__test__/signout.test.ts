f1aaf6bb75ffa87d29595e8b67e800885dc7907b
nikachello <nikain35@gmail.com>
2024-07-13 19:39:28 +0400

    finished signout and currentuser tests

---
+++ auth/src/routes/__test__/signout.test.ts
@@ -0,0 +1,14 @@
+import { app } from "../../app";
+import request from "supertest";
+
+it("clears the cookie after signing out", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com", password: "password" })
+    .expect(201);
+
+  const response = await request(app)
+    .post("/api/users/signout")
+    .send({})
+    .expect(200);
+});
