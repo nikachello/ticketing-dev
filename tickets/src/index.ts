6583f93b32e665947273b3bc00cb4b2d9ed0f48e
nikachello <nikain35@gmail.com>
2024-07-18 03:13:38 +0400

    added singleton of nats

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -9,12 +10,15 @@ const start = async () => {
   if (!process.env.MONGO_URI) {
     throw new Error("Mongo URI should be present");
   }
+
   try {
+    await natsWrapper.connect("ticketing", "qwerty", "http://nats-srv:4222");
     await mongoose.connect(process.env.MONGO_URI!);
     console.log("Connected to mongoDB");
   } catch (err) {
     console.log(err);
   }
+
   app.listen(3000, () => {
     console.log("Listening on port 3000!");
   });
