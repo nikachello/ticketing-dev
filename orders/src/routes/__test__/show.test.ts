560dd468e25b47bb2df4be1710a2b223a4961726
nikachello <nikain35@gmail.com>
2024-07-19 07:49:26 +0400

    finished showing with tests

---
+++ orders/src/routes/__test__/show.test.ts
@@ -0,0 +1,32 @@
+import request from "supertest";
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+
+it("fetches the order", async () => {
+  const user = await (global as any).signin();
+
+  // Create a ticket
+  const ticket = Ticket.build({
+    title: "title",
+    price: 20,
+  });
+  await ticket.save();
+
+  // Make a request to build an order with this ticket
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  console.log(order.order.id);
+
+  // make a request to fetch the order
+  const { body: fetchedOrder } = await request(app)
+    .get(`/api/orders/${order.order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(200);
+
+  expect(fetchedOrder.order.id).toEqual(order.order.id);
+});
--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,24 @@
 import express, { Request, Response } from "express";
+import { NotAuthorizedError, requireAuth } from "@chello12/common";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.get("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.get(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId).populate("ticket");
+    if (!order) {
+      res.status(404).send({ msg: ["Not found"] });
+    }
+
+    if (order!.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    res.send({ order });
+  }
+);
 
 export { router as showOrderRouter };
