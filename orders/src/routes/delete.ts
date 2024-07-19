fd988c407da7b84adf99e83c53e994bce9ae728f
nikachello <nikain35@gmail.com>
2024-07-19 20:21:12 +0400

    added event publishing

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,6 +1,9 @@
 import { NotAuthorizedError, requireAuth } from "@chello12/common";
 import express, { Request, Response } from "express";
 import { Order, OrderStatus } from "../models/order";
+import { OrderCancelledEvent } from "@chello12/common";
+import { natsWrapper } from "../nats-wrapper";
+import { OrderCancelledPublisher } from "../events/order-cancelled-publisher";
 
 const router = express.Router();
 
@@ -10,7 +13,7 @@ router.delete(
   async (req: Request, res: Response) => {
     const { orderId } = req.params;
 
-    const order = await Order.findById(orderId);
+    const order = await Order.findById(orderId).populate("ticket");
 
     if (!order) {
       res.status(404).send({ msg: ["Not found"] });
@@ -20,6 +23,14 @@ router.delete(
     }
     order!.status = OrderStatus.Cancelled;
     await order?.save();
+
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order?.id,
+      ticket: {
+        id: order?.ticket.id,
+        price: order!.ticket.price,
+      },
+    });
 
     res.status(204).send(order);
   }
