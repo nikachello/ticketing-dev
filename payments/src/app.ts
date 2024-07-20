7c701176816fe5f45061086254d5e9af1356a5b9
nikachello <nikain35@gmail.com>
2024-07-21 02:44:34 +0400

    initial setup for payments service

---
+++ payments/src/app.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+
+const app = express();
+
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+
+export { app };
---
+++ payments/src/test/setup.ts
@@ -0,0 +1,47 @@
+import mongoose, { mongo } from "mongoose";
+import jwt from "jsonwebtoken";
+
+jest.mock("../nats-wrapper.ts");
+
+beforeAll(async () => {
+  jest.setTimeout(60000); // 60 seconds timeout
+  process.env.JWT_KEY = "asdf";
+
+  const mongoUri = "mongodb://localhost:27017/test"; // Local MongoDB instance
+  await mongoose.connect(mongoUri);
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
+
+(global as any).signin = async () => {
+  // Build a JWT payload {id, email}
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: "guest@guests.com",
+  };
+
+  // Create the jwt
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  // Build session Object. {jwt: my_jwt}
+  const session = { jwt: token };
+
+  // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
+  // return a string thats the cookie with the encoded data
+  return `session=${base64}`;
+};
