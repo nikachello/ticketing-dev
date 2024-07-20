ce95cfc8d435080fca3f9051e8521ff66361980e
nikachello <nikain35@gmail.com>
2024-07-20 19:22:16 +0400

    expiration service initial commit

---
+++ expiration/src/index.ts
@@ -0,0 +1,40 @@
+import { natsWrapper } from "./nats-wrapper";
+
+const start = async () => {
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS cluster ID should be present");
+  }
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS client ID should be present");
+  }
+
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS url should be present");
+  }
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed");
+      process.exit();
+    });
+
+    natsWrapper.client.on("disconnect", () => {
+      console.log("disconnected");
+      process.exit();
+    });
+
+    process.on("SIGINT", () => natsWrapper.client!.close());
+    process.on("SIGTERM", () => natsWrapper.client!.close());
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
